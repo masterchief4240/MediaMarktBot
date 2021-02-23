2a52efa1b54418853b89ead502e6be34c47401be
Matthias Gieseke <matthias.gieseke1@gmail.com>
2021-02-23 16:20:37 +0100

    Add project files.

---
+++ MediaMarktBot/MediaMarktXboxBot.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace MediaMarktBot 
+{
+    public partial class MediaMarktXboxBot : Form
+    {
+        public MediaMarktXboxBot()
+        {
+            InitializeComponent();
+        }
+
+        // initiate browser, etc.
+        public static IWebDriver driver = new FirefoxDriver();
+        public WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(300));
+        public IWebElement element;
+
+
+
+        // start process
+        public void button1_Click(object sender, EventArgs e)
+        {
+            driver.Manage().Cookies.DeleteAllCookies();
+
+            // open paypal.com
+            driver.Url = "https://www.paypal.com/de/signin";
+
+            // click on "Accept all cookies" in paypal to finish payment
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("acceptAllButton")));
+            element = driver.FindElement(By.Id("acceptAllButton"));
+            element.Click();
+
+            // wait until user logged in and notifications from homescreen are visible
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("header-notifications")));
+
+            // Open media markt website after successful paypal login
+            driver.Url = "https://www.mediamarkt.de/";
+
+            // click on privacy accept button
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("privacy-layer-accept-all-button")));
+            element = driver.FindElement(By.Id("privacy-layer-accept-all-button"));
+            element.Click();
+
+            // MediaMarkt Login; open "Mein Konto" dropdown
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/div[2]/div[2]/header/div/div[2]/div[5]/button[1]/div/span[1]")));
+            element = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[2]/header/div/div[2]/div[5]/button[1]/div/span[1]"));
+            element.Click();
+            //click on "Anmelden"
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[1]/div[2]/div[2]/header/div/div[2]/div[5]/div/div/div/button")));
+            element = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[2]/header/div/div[2]/div[5]/div/div/div/button"));
+            element.Click();
+            // wait until login windows disappeared / user logged in and entered account details
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.InvisibilityOfElementLocated(By.XPath("/html/body/div[1]/div[1]/div/div[2]")));
+
+            // go to xbox article site
+            driver.Url = "https://www.mediamarkt.de/de/product/_microsoft-xbox-series-x-1-tb-2677360.html";
+
+            // start refresh cycle until article is in stock
+            timer1.Start();
+        }
+
+
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            if (driver.FindElement(By.XPath("//*[contains(text(),'Dieser Artikel ist aktuell nicht verfügbar.')]")).Displayed | driver.FindElement(By.XPath("//*[contains(text(),'Leider keine Lieferung möglich')]")).Displayed)
+            {
+                driver.Navigate().Refresh();
+            }
+            else
+            {
+                timer1.Stop();
+                BuyArticle();
+            }
+        }
+
+
+
+        // Buy Xbox since it's confirmed to be in stock
+        void BuyArticle()
+        {
+
+            // click on "In den Warenkorb"
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("pdp-add-to-cart-button")));
+            element = driver.FindElement(By.Id("pdp-add-to-cart-button"));
+            element.Click();
+
+            // click on "Zum Warenkorb"
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//*[contains(text(),'Zum Warenkorb')]")));
+            element = driver.FindElement(By.XPath("//*[contains(text(),'Zum Warenkorb')]"));
+            element.Click();
+
+            // click on "Zur Kasse gehen"
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//*[contains(text(),'Zur Kasse gehen')]")));
+            element = driver.FindElement(By.XPath("//*[contains(text(),'Zur Kasse gehen')]"));
+            element.Click();
+
+            // click on "Weiter" button while Paypal is automatically chosen (since its the first entry in the payment option list
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector("#root > div.indexstyled__StyledAppWrapper-sc-1hu9cx8-0.klAfyt > div.CheckoutPageWrapper__StyledCheckoutPage-sc-10wdmdy-0.huVVXd > div.Grid__StyledGrid-fs0zc2-0.cQIsoQ.CheckoutPageWrapper__StyledGrid-sc-10wdmdy-1.foupww > div > div:nth-child(2) > div > div.Cellstyled__StyledCell-sc-1wk5bje-0.iNYTnU.StepWrapper__StyledSummary-sc-8bgpux-4.gHCmSn > div > div > div > div > div.BasketResume__StyledSummaryButtons-l2uuxo-0.kUGHLa > div > button")));
+            element = driver.FindElement(By.CssSelector("#root > div.indexstyled__StyledAppWrapper-sc-1hu9cx8-0.klAfyt > div.CheckoutPageWrapper__StyledCheckoutPage-sc-10wdmdy-0.huVVXd > div.Grid__StyledGrid-fs0zc2-0.cQIsoQ.CheckoutPageWrapper__StyledGrid-sc-10wdmdy-1.foupww > div > div:nth-child(2) > div > div.Cellstyled__StyledCell-sc-1wk5bje-0.iNYTnU.StepWrapper__StyledSummary-sc-8bgpux-4.gHCmSn > div > div > div > div > div.BasketResume__StyledSummaryButtons-l2uuxo-0.kUGHLa > div > button"));
+            element.Click();
+
+            // click on "Fortfahren und bezahlen"
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector("#root > div.indexstyled__StyledAppWrapper-sc-1hu9cx8-0.klAfyt > div.CheckoutPageWrapper__StyledCheckoutPage-sc-10wdmdy-0.huVVXd > div.Grid__StyledGrid-fs0zc2-0.cQIsoQ.CheckoutPageWrapper__StyledGrid-sc-10wdmdy-1.foupww > div > div:nth-child(2) > div > div.Cellstyled__StyledCell-sc-1wk5bje-0.iNYTnU.StepWrapper__StyledSummary-sc-8bgpux-4 > div > div > div > div > div.BasketResume__StyledSummaryButtons-l2uuxo-0.eVfzHQ > div > button")));
+            element = driver.FindElement(By.CssSelector("#root > div.indexstyled__StyledAppWrapper-sc-1hu9cx8-0.klAfyt > div.CheckoutPageWrapper__StyledCheckoutPage-sc-10wdmdy-0.huVVXd > div.Grid__StyledGrid-fs0zc2-0.cQIsoQ.CheckoutPageWrapper__StyledGrid-sc-10wdmdy-1.foupww > div > div:nth-child(2) > div > div.Cellstyled__StyledCell-sc-1wk5bje-0.iNYTnU.StepWrapper__StyledSummary-sc-8bgpux-4 > div > div > div > div > div.BasketResume__StyledSummaryButtons-l2uuxo-0.eVfzHQ > div > button"));
+            element.Click();
+
+            // click on "Pay now" in paypal to finish payment
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("payment-submit-btn")));
+            element = driver.FindElement(By.Id("payment-submit-btn"));
+            System.Threading.Thread.Sleep(8000);
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(element);
+            actions.Perform();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("payment-submit-btn")));
+            element = driver.FindElement(By.Id("payment-submit-btn"));
+            element.Click();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("README.txt");
+        }
+    }
+}
---
+++ MediaMarktBot/README.txt
@@ -0,0 +1,65 @@
+MediaMarktBot Readme
+
+GERMAN / DEUTSCH
+
+Vorrausetzungen:
+
+- Installiere den Firefox Webbrowser 
+https://www.mozilla.org/de/firefox/new/
+
+- Installiere geckodriver
+https://github.com/mozilla/geckodriver/releases
+Nachdem du geckodriver.exe heruntergeladen hast, verschiebe diese einfach in den folgenden Ordner:
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps
+
+- Logge dich in dein Media Markt Konto ein und stelle sicher, dass der Warenkorb komplett leer ist, bevor du den Bot benutzt.
+Der Kaufprozess wird stecken bleiben, sollten sich bereits Artikel im Warenkorb befinden.
+
+- Du musst bei Paypal eine Zahlungsart hinterlegt haben (Kreditkarte, Girokonto), die standardmäßig ausgewählt wird
+
+
+Anmerkungen:
+
+- Soweit ich es getestet habe kannst du den Bot 48 Stunden laufen lassen, danach laufen die Paypal Cookies ab und du musst das Programm beenden und den ganzen Prozess neu starten
+
+- Wenn du während des Login Prozesses zu lange brauchst, kommt ein Error / eine Exception und du musst von vorne starten
+
+
+HOW TO:
+
+Der Bot funktioniert vollautomatisch. Nach dem starten leitet er dich auf Paypal, wo du dich einloggen musst.
+Danach Media Markt, erneut einloggen. Als nächstes geht er auf die Produktseite der Xbox Series X bei Media Markt und aktualisiert diese alle 10 Sekunden.
+Sobald der Artikel auf Lager ist, geht es zum Checkout und es wird direkt über Paypal bezahlt.
+
+
+
+ENGLISH
+
+Requirements:
+
+- Install the Firefox web browser
+https://www.mozilla.org/en-US/firefox/new/
+
+- Install geckodriver
+https://github.com/mozilla/geckodriver/releases
+After you've downloaded geckodriver.exe, just move it to the following folder:
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps
+
+- Log into your Media Markt account and make sure that the shopping cart is completely empty before you use the bot.
+The purchase process will get stuck if there are already items in the shopping cart.
+
+- You must have saved a payment method with PayPal (credit card, current account), which is selected by default
+
+
+Remarks:
+
+- As far as I have tested it, you can let the bot run for 48 hours, after which the PayPal cookies will expire and you have to exit the program and restart the whole process
+
+- If you take too long during the login process, you will get an error / exception and have to start over
+
+
+HOW TO:
+
+The bot works fully automatically. After starting, it will direct you to PayPal, where you have to log in.
+Then log in to Media Markt again. Next he goes to the Xbox Series X product page at Media Markt and updates it every 10 seconds.
+As soon as the article is in stock, it goes to the checkout and is paid directly via PayPal.
